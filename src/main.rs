2818ce5914906e72634b7c5ebb87e69c24664f20
Mateusz Grzybowski <mateusz.f.grzybowski@gmail.com>
2025-09-02 23:33:10 +0100

    Start implementing Lox

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std;
+use std::io::Error;
+use std::process::exit;
+
+fn run_file(path: &str) -> Result<String, Error> {
+    let contents = std::fs::read_to_string(path)?;
+    Ok(contents)
+}
+
+fn run_prompt() {}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() > 2 {
+        println!("Usage: jlox [script]");
+        exit(64);
+    } else if args.len() == 2 {
+        run_file(&args[1]).unwrap();
+    } else {
+        run_prompt();
+    }
 }
